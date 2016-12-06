56527ced0fab6f9d331336d5cbc5208d7dd89922
sriramab <b.srirama@gmail.com>
2016-12-06 22:57:21 +0100

    clean setup, all variables read in and no clutter of intermediate variables Fixes #1 and Fixes #4

--- R/cchaid.R
+++ R/cchaid.R
@@ -3,27 +3,27 @@
 cc_define_file<-function(x) {
   read_file<-x
   cc_read_file_name(read_file)
-
+  
 }
 
 # this function reads the files
 cc_read_file_name<-function(y){
   #read file data into R
-  f_read_with_Header<<-read.csv(y, header=F, sep="\t")
-  f_read_without_Header<<-read.csv(y, header=T, sep="\t", skip = 1)
-  f_inclusionAs<<-t(f_read_with_Header[1,])
-  f_columnsAs<<-t(f_read_with_Header[2,])
+  f_read_with_Header<-read.csv(y, header=F, sep="\t")
+  f_read_without_Header<-read.csv(y, header=T, sep="\t", skip = 1)
+  f_inclusionAs<-t(f_read_with_Header[1,])
+  f_columnsAs<-t(f_read_with_Header[2,])
   ignored<-table(f_inclusionAs)
   
   print(paste0("Total variables read: ", length(f_inclusionAs), " ignored: ", ignored[["x"]][1]))
   #cc_dataset<<-data.frame(1:length(f_read_without_Header[[1]]))
   nr<-seq(1,length(f_read_without_Header[[1]]))
-  cc_dataset<<-data.frame(nr)
-  cc_dataset_names<<-vector()
-  cc_list<<-list()
+  cc_dataset<-data.frame(nr)
+  cc_dataset_names<-vector()
+  cc_list<-list()
   for (i in 1:22){
     #print(i)
-    data_type_by_user<<-f_inclusionAs[i]
+    data_type_by_user<-f_inclusionAs[i]
     #print(data_type_by_user)
     cc_dataset_names[1] <-"Nr"
     switch(data_type_by_user,
@@ -32,27 +32,27 @@ cc_read_file_name<-function(y){
              #create nominal variables
              aa<-f_columnsAs[i]
              #print(paste0("   ",aa))
-             cc_list[[i]]<<-as.factor(f_read_without_Header[[aa]])
-             cc_dataset<<-cbind(cc_dataset,cc_list[[i]])
-             cc_dataset_names[i+1]<<-aa
+             cc_list[[i]]<-as.factor(f_read_without_Header[[aa]])
+             cc_dataset<-cbind(cc_dataset,cc_list[[i]])
+             cc_dataset_names[i+1]<-aa
            },
            "o"={
              #create ordered variables
              aa<-f_columnsAs[i]
              #print(paste0("   ",aa))
-             cc_list[[i]]<<-as.ordered(f_read_without_Header[[aa]])
-            # cc_dataset<<-data.frame(as.ordered(f_read_without_Header[[aa]]))
-             cc_dataset<<-cbind(cc_dataset,cc_list[[i]])
-             cc_dataset_names[i+1]<<-aa
+             cc_list[[i]]<-as.ordered(f_read_without_Header[[aa]])
+             # cc_dataset<<-data.frame(as.ordered(f_read_without_Header[[aa]]))
+             cc_dataset<-cbind(cc_dataset,cc_list[[i]])
+             cc_dataset_names[i+1]<-aa
            },
            "c"={
              #create continuous variables
              aa<-f_columnsAs[i]
              #print(paste0("   ",aa))
-             cc_list[[i]]<<-as.numeric(f_read_without_Header[[aa]])
+             cc_list[[i]]<-as.numeric(f_read_without_Header[[aa]])
              # cc_dataset<<-data.frame(as.ordered(f_read_without_Header[[aa]]))
-             cc_dataset<<-cbind(cc_dataset,cc_list[[i]])
-             cc_dataset_names[i+1]<<-aa
+             cc_dataset<-cbind(cc_dataset,cc_list[[i]])
+             cc_dataset_names[i+1]<-aa
              #cc_dataset_names<-c(cc_dataset_names,aa)
            },
            "x"={
@@ -64,26 +64,28 @@ cc_read_file_name<-function(y){
            {
              #print("default")
            }
-      
+           
     )
-  
+    
   }
   
- 
+  
   for (i in 1:19){
-   # print(paste0(i,"--", length(cc_list[[i]])))
+    # print(paste0(i,"--", length(cc_list[[i]])))
   }
-  cc_prepare_data_frame()
-  }
+  #cc_dataset<<-cc_dataset
+  cc_prepare_data_frame(cc_dataset_names,cc_dataset)
+}
 
-cc_prepare_data_frame<-function(){
+cc_prepare_data_frame<-function(x,y){
   #print("hello")
-  cc_names<<-na.omit(cc_dataset_names)
-  cc_names<<-c("Nr", cc_names)
-  colnames(cc_dataset)<<-cc_names
+  cc_names<-na.omit(x)
+  cc_names<-c( cc_names)
+  colnames(y)<-cc_names
+  return(y)
   
   
 }
-#cc_define_file("aidwork3.txt") # release this line when testing
+#mydata<-cc_define_file("aidwork3.txt") # release this line when testing
 
 
