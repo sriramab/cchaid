2d0f1b281abdcdc5487ecae531bddbc4aef3875e
sriramab <b.srirama@gmail.com>
2016-12-06 16:31:04 +0100

    Fixes #2

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Household survey data
+#'
+#' Data from a household survey in 2004
+#'
+#' @docType data
+#'
+#' @usage data(cc_demo_data)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Srirama
+#' (\href{http://github.com/sriramb/cchaid}{Srirama})
+#'
+#' @source \href{http://github.com/sriramb/cchaid}{Srirama}
+#'
+#' @examples
+#' data(cc_demo_data)
+#' 
+"cc_demo_data"
