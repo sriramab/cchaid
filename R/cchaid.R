03229704e6aa0c9b0d57aa48f323bb1785ce6563
sriramab <b.srirama@gmail.com>
2016-12-06 10:57:15 +0100

    gives a clean data cc_dataset() frame based on user definitions of what to include and excluse and which are ordinal, nominal, continuous and deleted.

--- R/cchaid.R
+++ R/cchaid.R
@@ -14,46 +14,49 @@ cc_read_file_name<-function(y){
   f_inclusionAs<<-t(f_read_with_Header[1,])
   f_columnsAs<<-t(f_read_with_Header[2,])
   ignored<-table(f_inclusionAs)
-  #ignored<-sum(unlist(f_datatypeList), "x")
-  #class(f_datatypeList)
+  
   print(paste0("Total variables read: ", length(f_inclusionAs), " ignored: ", ignored[["x"]][1]))
-  cc_dataset<<-data.frame(1:length(f_read_without_Header[[1]]))
+  #cc_dataset<<-data.frame(1:length(f_read_without_Header[[1]]))
+  nr<-seq(1,length(f_read_without_Header[[1]]))
+  cc_dataset<<-data.frame(nr)
   cc_dataset_names<<-vector()
   cc_list<<-list()
   for (i in 1:22){
     print(i)
     data_type_by_user<<-f_inclusionAs[i]
     print(data_type_by_user)
+    cc_dataset_names[1] <-"Nr"
     switch(data_type_by_user,
            
            "n"={
-             #create nominal variable
+             #create nominal variables
              aa<-f_columnsAs[i]
              print(paste0("   ",aa))
              cc_list[[i]]<<-as.factor(f_read_without_Header[[aa]])
              cc_dataset<<-cbind(cc_dataset,cc_list[[i]])
-             #cc_dataset_names[i]<-aa
+             cc_dataset_names[i+1]<<-aa
            },
            "o"={
-             #create nominal variable
+             #create ordered variables
              aa<-f_columnsAs[i]
              print(paste0("   ",aa))
              cc_list[[i]]<<-as.ordered(f_read_without_Header[[aa]])
             # cc_dataset<<-data.frame(as.ordered(f_read_without_Header[[aa]]))
              cc_dataset<<-cbind(cc_dataset,cc_list[[i]])
-            # cc_dataset_names<-c(cc_dataset_names,aa)
+             cc_dataset_names[i+1]<<-aa
            },
            "c"={
-             #create nominal variable
+             #create continuous variables
              aa<-f_columnsAs[i]
              print(paste0("   ",aa))
              cc_list[[i]]<<-as.numeric(f_read_without_Header[[aa]])
              # cc_dataset<<-data.frame(as.ordered(f_read_without_Header[[aa]]))
              cc_dataset<<-cbind(cc_dataset,cc_list[[i]])
+             cc_dataset_names[i+1]<<-aa
              #cc_dataset_names<-c(cc_dataset_names,aa)
            },
            "x"={
-             #create nominal variable
+             #list deleted variables
              aa<-f_columnsAs[i]
              print(paste0(" to be deleted  ",aa))
              #cc_dataset<<-data.frame(as.factor(f_read_without_Header[[aa]]))
@@ -63,9 +66,10 @@ cc_read_file_name<-function(y){
            }
       
     )
+  
   }
- # cc_dataset<-data.frame(cc_list)
-  #cc_dataset  <-  as.data.frame(matrix(unlist(cc_list), nrow=length(unlist(cc_list[[1]]))))
+  
+ 
   for (i in 1:19){
     print(paste0(i,"--", length(cc_list[[i]])))
   }
@@ -74,9 +78,12 @@ cc_read_file_name<-function(y){
 
 cc_prepare_data_frame<-function(){
   print("hello")
-  #cc_imported_data
+  cc_names<<-na.omit(cc_dataset_names)
+  cc_names<<-c("Nr", cc_names)
+  colnames(cc_dataset)<<-cc_names
+  
   
 }
-#a<-cc_define_file("aidwork3.txt")
+#cc_define_file("aidwork3.txt") # release this line when testing
 
 
