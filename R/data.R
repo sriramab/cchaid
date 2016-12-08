c11f7b2d2019ee87fd5015feb8509d7e5a866d8b
sriramab <b.srirama@gmail.com>
2016-12-08 22:28:41 +0100

    Fixes #6

--- R/data.R
+++ R/data.R
@@ -1,20 +1,21 @@
-#' Household survey of 2004
+#' Household Survey of 2004
 #'
-#' A dataset containing the interviews of 54,000
-#'  participants The variables are as follows:
+#' A dataset containing the interviews of 11000
+#'  participants. The variables are as follows:
 #'
-#' @format A data frame with 53940 rows and 10 variables:
+#' @format Socio-economic and situational attributes used:
 #' \describe{
-#'   \item{Urb}{Urban Residence}
-#'   \item{Comp}{weight }
-#'   \item{Child}{quality }
-#'   \item{Day}{ colour, }
-#'   \item{pAge}{a measurement  }
-#'
-#'   \item{SECx}{length}
-#'   \item{Ncar}{width }
-#'   \item{Gend}{depth }
-#'   \item{Driver}{total z }
-#'   \item{Dur}{width of top }
+#'   \item{Urb}{Urban Density}
+#'   \item{Comp}{Household Composition }
+#'   \item{Child}{Child category }
+#'   \item{Day}{Day of the week, }
+#'   \item{pAge}{Age category }
+#'   \item{wstat}{Work status of an individual}
+#'   \item{wstat}{Work status of partner}
+#'   \item{SECx}{Socio-economic class}
+#'   \item{Ncar}{Number of cars}
+#'   \item{Gend}{Gender}
+#'   \item{Driver}{Has a driving license}
+#'   \item{Dur}{Duration}
 #' }
 "cc_dataset"
