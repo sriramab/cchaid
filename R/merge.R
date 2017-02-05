7d4b654d202764542e8bcf58bac2a69437436bed
sriramab <b.srirama@gmail.com>
2017-02-05 18:29:24 +0100

    opted for the switch statement instead of sapply

--- R/merge.R
+++ R/merge.R
@@ -10,74 +10,7 @@
 
 
 
-ccmerge1<-function(data=x, alpha_merge=0.05){
-  #print(paste(names(data), " -- ", alpha_merge))
-  if(!is.data.frame(data)) stop("data is not a dataframe", call. = TRUE)
-  if(alpha_merge>1) warning("p value provided for alpha_merge should be <=1", call. = TRUE)
-  
-  # Detect each column on its data_type -> function to be used is class; 
-  # and send it to its appropriate function for merging
-  
-  sapply( data, function(x) if(class(x)[1]=="factor"){
-    # merge for nominal values
-    nominal_merge(x)
 
-  } else if(class(x)[1]=="ordered") {
-    # merge for ordinal values
-
-    ordinal_merge(x)
-
-  } #else{
-      #print(paste("x"))
-    #}
-  )
-  
-  
-  
-  
-  
-}
-
-nominal_merge<-function(y){
-  
-  l=length(levels(y))
-  print(paste("Predictor has ", l, " levels"))
-}
-
-ordinal_merge<-function(y){
-  #print(paste("inside ordinal merge=", a10))
-  l=length(levels(y))
-  print(paste("Predictor has ", l, " levels"))
-}
-
-
-ccmerge2<-function(data=d, alpha_merge=0.05){
-  #print(paste(names(data), " -- ", alpha_merge))
-  if(!is.data.frame(data)) stop("data is not a dataframe", call. = TRUE)
-  if(alpha_merge>1) stop("p value provided for alpha_merge should be <=1", call. = TRUE)
-  
-  # Detect each column on its data_type -> function to be used is class; 
-  # and send it to its appropriate function for merging
-  
-  sapply( colnames(data), function(x) if(class(data[,x])[1]=="factor"){
-    # merge for nominal values
-    nominal_merge(data[,x])
-    
-  } else if(class(data[,x])[1]=="ordered") {
-    # merge for ordinal values
-    
-    ordinal_merge(data[,x])
-    
-  } #else{
-  #print(paste("x"))
-  #}
-  )
-  
-  
-  
-  
-  
-}
 
 ccmerge<-function(data=x, alpha_merge=0.05){
   #print(paste(names(data), " -- ", alpha_merge))
@@ -117,5 +50,5 @@ o_merge<-function(y){
   l=length(levels(y))
   print(paste("Predictor has ", l, " levels"))
 }
-ccmerge3(a)
+
 ccmerge(a)
