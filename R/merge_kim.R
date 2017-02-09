d7df86c61dc609e8118a1d18a9579ecd09e0a5c1
tpgjs66 <tpgjs66@users.noreply.github.com>
2017-02-09 16:57:36 +0100

    Create merge_kim.R
    merging loop seems to work now.

---
+++ R/merge_kim.R
@@ -0,0 +1,133 @@
+## THIS FILE IS RESTRICTED TO MERGING OF PREDICTORS,
+# F test is the square of t test when compairing a pair
+# this function ccmerge gets the DATAFRAME from the user, merges insignificant levels
+#' This function merges insignificant categories(levels) of each column within a dataframe
+#'
+#' @param x a dataframe from user, as constructed using cc_read_file()
+#' @return Not yet, but should return a list of lists of dataframes.
+#' @export
+#' @examples
+#' devtools::install_github("sriramab/cchaid")
+#' library(cchaid)
+#' a=cc_dataset
+#' ccmerge(a)
+
+##--------------------------------------------------------------------------##
+#                                                                            #
+#                       MAIN FUNCTION CALLED BY THE USER                     #
+#                                                                            #
+##--------------------------------------------------------------------------##
+
+ccmerge <- function(data = x,
+                    alpha_merge = 0.05,
+                    alpha_split = 0.05) {
+  #print(paste(names(data), " -- ", alpha_merge))
+  if (!is.data.frame(data))
+    stop("data is not a dataframe", call. = TRUE)
+  if (alpha_merge > 1)
+    warning("p value provided for alpha_merge should be <=1", call. = TRUE)
+  
+  # Detect each column on its data_type -> function to be used is class;
+  # and send it to its appropriate function for merging
+  n <- colnames(data[, -c(1, ncol(data))])
+  print(n)
+  
+  z<-c(0)
+  p_aov_val <- vector("numeric")
+  
+  for (i in n) {
+    p_aov<-merging_loop(data, data[, i], i, alpha_merge, alpha_split)[1] #Get adj. p-values with Bonferroni correction, which is an output of "merging_loop" function
+    #print(p_aov)
+    
+    z<-z+1
+    p_aov_val[z]<-p_aov
+   
+   print(p_aov_val)
+   cat("\n")
+  }
+}
+
+##--------------------------------------------------------------------------##
+# FUNCTION BELOW MERGES INSIGNIFICANT CATEGORIES WITHIN EACH nominal COLUMN data[,i]  
+# OF DATAFRAME data AND SPITS OUT ADJ. P-VALUE WITH BONFERRONI CORRECTION
+##--------------------------------------------------------------------------##
+
+merging_loop <- function(data, y, i, alpha_merge, alpha_split) {
+  
+  l = length(levels(data[,i]))  # of categories in a predictor data[,i]
+  
+  print(paste(colnames(data[i]), " has ", l, " levels"))
+  cat("\n")
+  
+  if (l <= 2){
+    p_aov = as.matrix(summary((aov(data[, ncol(data)]~data[,i],data)))[[1]][,5])
+    print(paste("========================Finished Merging loop=========================="))
+    print(paste("Adjusted p-value with bonferroni correction:",p_aov[1]))
+    cat("\n\n")
+  }else{
+    k<-c(0)    #index k for loop
+    repeat{     #Do merging while l=>2 or p < alpha_merge
+     
+      k<-k+1   
+      
+      p = (pairwise.t.test(data[, ncol(data)],data[,i],p.adjust.method = "none",paired = FALSE,pool.sd = FALSE,var.equal = TRUE))$p.value
+      
+      if(class(data[,i])[1]=="factor"){
+        p_max_value = max(p, na.rm = TRUE) # This picks the max p value for nominal predictor, which means the least significant pair.
+      }else if(class(data[,i])[1]=="ordered"){
+        p_max_value = max(diag(p), na.rm = TRUE) # This picks the max p value ordinal predictor, which means the least significant pair.
+      }
+      
+      # print(p)
+      # print(p_max_value)
+      # print(alpha_merge)
+      
+      if(l==2|(p_max_value < alpha_merge)) break
+      
+      p_max = which(p == max(p, na.rm = TRUE), arr.ind = TRUE) 
+      r <- rownames(p)[p_max[, 1]]
+      c <- colnames(p)[p_max[, 2]]
+      nameofMergedCategory <- paste(r, c, sep = "-") #------- MERGED HERE WITH A HYPHEN
+      
+      print(paste("Merged categories in",k,"-th Merging loop:", nameofMergedCategory))
+      
+      # CALL MERGING FUNCTION merging_function()
+      data[,i]<-merging_function(data, y, r, c, nameofMergedCategory, i)
+      
+      l = length(levels(data[,i]))
+      print(paste("Total number of categories after",k,"-th Merging loop:", l))
+      
+      print(paste("========================end of",k,"-th Merging loop=========================="))
+      cat("\n")
+      
+    }
+    
+    p_aov = as.matrix(summary((aov(data[, ncol(data)]~data[,i],data)))[[1]][,5])
+    print(paste("======================Finished Merging loop for '",colnames(data[i]),"'====================="))
+    print(paste("Adjusted p-value with bonferroni correction:",p_aov[1]))
+    cat("\n\n")
+    
+  }
+  return(p_aov)
+}
+
+
+merging_function <- function(data, y, r, c, nameofMergedCategory, i) {
+  
+  list_of_levels = levels(data[,i])
+  #print(list_of_levels)
+  
+  for (q in seq_along(list_of_levels)) {
+    if (list_of_levels[q] == r || list_of_levels[q] == c) {
+      list_of_levels[q] = nameofMergedCategory
+    }
+  }
+  levels(data[,i]) <- list_of_levels
+  
+  #cat("\n")
+  
+  return(data[,i])
+}
+
+
+ccmerge(a)
